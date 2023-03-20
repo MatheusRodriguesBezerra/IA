09155b6f879a707e4aee0fe337a7d026d78fb182
dRotnes <rotnesdante10@gmail.com>
2023-03-20 18:01:03 +0000

    Finalized testing of full columns

--- trabalho2/connectFour.py
+++ trabalho2/connectFour.py
@@ -1,120 +1,58 @@
 from tabuleiro import Tabuleiro
 
-def botPlays(node: Tabuleiro, n: int):
+def makeMove(node: Tabuleiro, n: int, player:str):
     new_table = list(map(list, node.getGame()))
     n = n - 1
-    for i in range(len(node.getGame())):
-        if (new_table[i][n] is not None):
-            i = i - 1
-            break
-    new_table[i][n] = 'X'
-    new_table = Tabuleiro(new_table)
-    return new_table
-
-def playerPlays(node: Tabuleiro, n: int):
-    new_table = list(map(list, node.getGame()))
-    n = n - 1
-    for i in range(len(node.getGame())):
+    if(node.getColumnsDone()[n]):
+        print("Coluna escolhida está cheia!\n")
+    for i in range(len(new_table)):
         if (new_table[i][n] !='-'):
             i = i - 1
             break
-    new_table[i][n] = 'O'
-    new_table = Tabuleiro(new_table)
-    return new_table
-
-def playerPlays2(node: Tabuleiro, n: int):
-    new_table = list(map(list, node.getGame()))
-    n = n - 1
-    for i in range(len(node.getGame())):
-        if (new_table[i][n] !='-'):
-            i = i - 1
-            break
-    new_table[i][n] = 'X'
+    if(player == 'BOT'):
+        new_table[i][n] = 'O'
+    else:
+        new_table[i][n] = 'X'
     new_table = Tabuleiro(new_table)
     return new_table
 
 def getActionsBot(node: Tabuleiro):
     actions = []
     for i in range(1,8,1):
-        actions.append(botPlays(node,i))
+        print(i)
+        if(node.getColumnsDone()[i-1] is False):
+            actions.append(makeMove(node,i, 'BOT'))
+    for i in actions:
+        print(i)
     return actions
 
-def pointsOfList(x:list) -> int:
-    if (x.count('O') == 3) and (x.count('X') == 0):
-        return -50
-    elif (x.count('O') == 2) and (x.count('X') == 0):
-        return -10
-    elif (x.count('O') == 1) and (x.count('X') == 0):
-        return -1
-    elif (x.count('O') > 0) and (x.count('X') > 0):
-        return 0
-    elif (x.count('O') == 0) and (x.count('X') == 0):
-        return 0
-    elif (x.count('O') == 0) and (x.count('X') == 1):
-        return 1
-    elif (x.count('O') == 0) and (x.count('X') == 2):
-        return 10
-    elif (x.count('O') == 0) and (x.count('X') == 3):
-        return 50
-    else:
-        return 0
-    
-def getPoints(node: Tabuleiro) -> int:
-    #PRINT
-    m = node.getGame()
-    count = 0
-    for i in range(0,6,1):     # sequência nas horizontais
-        for j in range(0,4,1):
-            l = [m[i][j],m[i][j+1],m[i][j+2],m[i][j+3]]
-            count = count + pointsOfList(l)
-
-    for i in range(0,3,1):     # sequência nas verticais
-        for j in range(0,7,1):
-            l = [m[i][j],m[i+1][j],m[i+2][j],m[i+3][j]]
-            count = count + pointsOfList(l)
-
-    for i in range(0,3,1):     # sequência nas diagonais positivas
-        for j in range(0,4,1):
-            l = [m[i][j],m[i+1][j+1],m[i+2][j+2],m[i+3][j+3]]  
-            count = count + pointsOfList(l)
-
-    for i in range(3,6,1):     # sequência nas diagonais negativas
-        for j in range(0,4,1):
-            l = [m[i][j],m[i-1][j+1],m[i-2][j+2],m[i-3][j+3]]
-            count = count + pointsOfList(l)
-
-    
-    return count
-
-
-
-# tab = [[None,None,None,None,None,None,None],
-#        [None,None,None,None,None,None,None],
-#        ['X', None,None, 'X',None, 'O',None],
-#        ['O', None, 'X', 'O',None, 'X', 'X'],
-#        ['X', None, 'X', 'O', 'O', 'X', 'X'],
-#        [None, None, 'X', None, 'X', 'X', 'X']]
-
-tab = [['-','-','-','-','-','-','-'],
-       ['-','-','-','-','-','-','-'],
-       ['-', '-','-', '-','-', '-','-'],
-       ['-', '-', '-', '-','-', '-', '-'],
-       ['-', '-', '-', '-', '-', '-', '-'],
-       ['-', '-', '-', '-', '-', '-', '-']]
+tab = [
+    ['X','-','-','-','-','-','X'],
+    ['X','-','-','-','-','-','X'],
+    ['X','-','-','-','-','-','X'],
+    ['X','-','-','-','-','-','X'],
+    ['X','-','-','-','-','-','X'],
+    ['X','-','-','-','-','-','X']
+]
 
 tab = Tabuleiro(tab)
-# print(tab.showGame())
-# print(getPoints(tab))
-# # botPlays(tab, 2)
-# c = getActionsBot(tab)
-# for item in c:
-#     print(item)
-#     print(getPoints(item))
-while(True):
-    print(tab) 
-    play = int(input("Which column u want to put:\n1-7\n"))
-    tab = playerPlays(tab, play)
-    print(tab)
+getActionsBot(tab)
+# for i in getActionsBot(tab):
+#     print(i)
+# while(True):
+#     print(tab)
+#     play = int(input("Which column u want to put:\n1-7\n"))
+#     tab = makeMove(tab, play, 'PLAYER')
+# print(tab.getPoints())
+# bot_plays = getActionsBot(tab)
+# 
 
-    play = int(input("Which column u want to put:\n1-7\n"))
-    tab = playerPlays2(tab, play)
+
+# while(True):
+#     print(tab) 
+#     play = int(input("Which column u want to put:\n1-7\n"))
+#     tab = playerPlays(tab, play)
+#     print(tab)
+
+#     play = int(input("Which column u want to put:\n1-7\n"))
+#     tab = playerPlays2(tab, play)
--- trabalho2/tabuleiro.py
+++ trabalho2/tabuleiro.py
@@ -1,14 +1,17 @@
 class Tabuleiro:
     def __init__(self, game):
         self.game = game
+        self.points = self.setBoardPoints()
+        self.colsDone = self.columnsFinished()
 
     def getGame(self):
-        nTabuleiro = Tabuleiro(self.game)
-        return nTabuleiro.game
-    
-    def showGame(self):
-        for i in self.getGame():
-            print(i)
+        return self.game
+
+    def getPoints(self):
+        return self.points
+
+    def getColumnsDone(self):
+        return self.colsDone
 
     def __str__(self):
         string = ""
@@ -20,4 +23,66 @@ class Tabuleiro:
         if(type(other) is not Tabuleiro):
             return False
         return self.game == other.game
+
+    def __pointsOfList(self, x:list) -> int:
+        num_O = x.count('O')
+        num_X = x.count('X')
+
+        if(num_X == 4):
+            return 512
+        elif(num_O == 4):
+            return -512
         
+        if(num_X == 0):
+            if(num_O == 3):
+                return -50
+            elif(num_O == 2):
+                return -10
+            elif(num_O == 1):
+                return - 1
+            else:
+                return 0
+        elif(num_O == 0):
+            if(num_X == 3):
+                return 50
+            elif(num_X == 2):
+                return 10
+            elif(num_X == 1):
+                return 1
+            else:
+                return 0
+        else:
+            return 0
+    
+    def setBoardPoints(self) -> int:
+        board = self.game
+        count = 0
+        for i in range(0,6,1):     # sequência nas horizontais
+            for j in range(0,4,1):
+                sequence_of_four_horizontal = [board[i][j],board[i][j+1],board[i][j+2],board[i][j+3]]
+                count = count + self.__pointsOfList(sequence_of_four_horizontal)
+
+        for i in range(0,3,1):     # sequência nas verticais
+            for j in range(0,7,1):
+                sequence_of_four_vertical = [board[i][j],board[i+1][j],board[i+2][j],board[i+3][j]]
+                count = count + self.__pointsOfList(sequence_of_four_vertical)
+
+        for i in range(0,3,1):     # sequência nas diagonais positivas
+            for j in range(0,4,1):
+                sequence_of_four_diagonal_1 = [board[i][j],board[i+1][j+1],board[i+2][j+2],board[i+3][j+3]]  
+                count = count + self.__pointsOfList(sequence_of_four_diagonal_1)
+
+        for i in range(3,6,1):     # sequência nas diagonais negativas
+            for j in range(0,4,1):
+                sequence_of_four_diagonal_2 = [board[i][j],board[i-1][j+1],board[i-2][j+2],board[i-3][j+3]]
+                count = count + self.__pointsOfList(sequence_of_four_diagonal_2)
+        return count
+
+    def columnsFinished(self):
+        colsDone = [False, False, False, False, False, False, False]
+        for i in range(len(self.game[0])):
+            col = [row[i] for row in self.game]
+            if(col.count('-') == 0):
+                colsDone[i] = True
+        return colsDone
+
