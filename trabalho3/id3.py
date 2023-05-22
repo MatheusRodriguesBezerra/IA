22b3d9afac47cc9271d04d1e2835c52072acdef6
dRotnes <rotnesdante10@gmail.com>
2023-05-22 23:02:15 +0100

    comit

--- trabalho3/id3.py
+++ trabalho3/id3.py
@@ -6,6 +6,7 @@ class Edge:
     def __init__(self, val):
         self.value = val
         self.child = None
+        self.total = 0
 
 class Node:
     def __init__(self, attribute):
@@ -16,48 +17,49 @@ class Node:
 
 class Tree:
     def __init__(self, data, target):
-        self.root = self.tree_build(data, target)
+        self.root = self.treeBuild(data, target)
 
-    def tree_build(self, data, target):
+    def treeBuild(self, data, target):
         root = Node(None)
-        if self.homogeneity(data, target):
+        if self.homogenity(data, target):
             root.final = True
             root.result = data[0][target]
             return root
         if len(data[0]) == 1:
             root.final = True
-            root.result = self.most_common(data, target)
+            root.result = self.mostCommon(data, target)
             return root
-        best_attribute = self.select_attribute(data, target)
+        best_attribute = self.selectAttribute(data, target)
         root.attribute = best_attribute
-        attribute_values = self.get_attribute_values(data, best_attribute)
+        attribute_values = self.getValue(data, best_attribute)
         for value in attribute_values:
             sub_data = self.getSubData(data, best_attribute, value)
-            child_node = self.tree_build(sub_data, target)
+            child_node = self.treeBuild(sub_data, target)
             edge = Edge(value)
             edge.child = child_node
             root.edges.append(edge)
         return root
 
     def predict(self, data):
-        return self.predict_recursive(self.root, data)
+        return self.predictRecursive(self.root, data)
 
-    def predict_recursive(self, node, data):
+    def predictRecursive(self, node, data):
         if node.final:
             return node.result
         for edge in node.edges:
             if data[node.attribute] == edge.value:
-                return self.predict_recursive(edge.child, data)
+                edge.total +=1
+                return self.predictRecursive(edge.child, data)
 
-    def homogeneity(self, data, target):
+    def homogenity(self, data, target):
         counter = Counter([record[target] for record in data])
         return len(counter.keys()) == 1
 
-    def most_common(self, data, target):
+    def mostCommon(self, data, target):
         counter = Counter([record[target] for record in data])
-        return counter.most_common(1)[0][0]
+        return counter.mostCommon(1)[0][0]
 
-    def get_attribute_values(self, data, attribute):
+    def getValue(self, data, attribute):
         return list(set([record[attribute] for record in data]))
 
     def getSubData(self, data, best_attribute, value):
@@ -71,9 +73,9 @@ class Tree:
             entropy -= p * math.log2(p)
         return entropy
 
-    def information_gain(self, data, attribute, target):
+    def gain(self, data, attribute, target):
         total_entropy = self.entropy(data, target)
-        values = self.get_attribute_values(data, attribute)
+        values = self.getValue(data, attribute)
         sub_entropy = 0
         for value in values:
             sub_data = self.getSubData(data, attribute, value)
@@ -81,14 +83,17 @@ class Tree:
             sub_entropy += p * self.entropy(sub_data, target)
         return total_entropy - sub_entropy
 
-    def select_attribute(self, data, target):
+    def selectAttribute(self, data, target):
         attributes = [a for a in data[0].keys() if a != target]
+        if list(data[0].keys())[0] in attributes:
+            attributes.remove(list(data[0].keys())[0])
         information_gains = {}
         for attribute in attributes:
-            information_gains[attribute] = self.information_gain(data, attribute, target)
+            information_gains[attribute] = self.gain(data, attribute, target)
         return max(information_gains, key=information_gains.get)
 
-def csv_to_dict(csvFilePath):
+
+def csvToDict(csvFilePath):
     jsonArray = []
 
     with open(csvFilePath, encoding='utf-8') as csvf:
@@ -98,7 +103,7 @@ def csv_to_dict(csvFilePath):
             jsonArray.append(row)
     return jsonArray
 
-def generate_output(root, indentation=''):
+def genOutput(root, indentation=''):
     output = ''
     if root.attribute is None:
         return ''
@@ -106,29 +111,30 @@ def generate_output(root, indentation=''):
         output += indentation + '<' + root.attribute + '>\n'
         if edge.child.final:
             output += indentation + ' ' + edge.value + ': '
-            output += edge.child.result + '\n'
+            output += edge.child.result + ' (total: ' + str(edge.total) + ')\n'
         else:
             output += indentation + ' ' + edge.value + ':\n'
-            output += generate_output(edge.child, indentation + '    ')
+            output += genOutput(edge.child, indentation + '    ')
     return output
 
-csvFilePath = r'./restaurant.csv'
-csvTestFilePath = r'./restaurant_test.csv'
-data = csv_to_dict(csvFilePath)
-test_data = csv_to_dict(csvTestFilePath)
+csvFilePath = r'./weather.csv'
+csvTestFilePath = r'./weather_test.csv'
+data = csvToDict(csvFilePath)
+test_data = csvToDict(csvTestFilePath)
 headers = list(data[0].keys())
 key_value = headers[-1]
 tree = Tree(data, key_value)
+
 
 for instance in test_data:
     prediction = tree.predict(instance)
     instance[key_value] = prediction
 
-output = generate_output(tree.root)
+output = genOutput(tree.root)
 print(output)
 
 #Escrever para csv
-with open('iris_result.csv', 'w') as file:
+with open('weather_result.csv', 'w') as file:
     writer = csv.DictWriter(file, fieldnames=headers, lineterminator = '\n')
     writer.writeheader()
     writer.writerows(test_data)
--- trabalho3/nodeAttribute.py
+++
@@ -1,9 +0,0 @@
-class NodeAttribute:
-    def __init__(self, name, edges, father = None):
-        self.father = father
-        self.name = name
-        self.edges = edges
-
-class Edge:
-    def __init__(self, name):
-        self.name = name
